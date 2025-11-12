7ac98ad528651f8f4cad9b2208d0057e9d103816
AlejoYarce <alejo.yarce990@gmail.com>
2025-11-12 10:32:07 -0500

    [DS-229] - adding avatar to the navbar (#14)

--- src/app/map/layout.tsx
+++ src/app/map/layout.tsx
@@ -4,7 +4,7 @@ export default function MapLayout({
   children: React.ReactNode
 }>) {
   return (
-    <div className='h-[calc(100vh-48px)] lg:h-[calc(100vh-96px)] 1121:h-[calc(100vh-48px)] w-full absolute top-12 lg:top-24 1121:top-12'>
+    <div className='h-[calc(100vh-48px)] lg:h-[calc(100vh-96px)] 1266:h-[calc(100vh-48px)] w-full absolute top-12 lg:top-24 1266:top-12'>
       {children}
     </div>
   )
