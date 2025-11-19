494e5adeca8d88bc2b2227a83d0ec4715f4369d7
Carlos Pesina <pesinasiller@users.noreply.github.com>
2025-11-19 22:59:00 +0400

    DS-158-hotjar_analytics (#17)
    Co-authored-by: José Carlos Pesina Siller <josecarlospesinasiller@Joses-MacBook-Air.local>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import Providers from '@/components/Providers'
 import { Footer } from '@/components/Footer'
 import Navbar from '@/components/Navbar'
+import Script from 'next/script'
 
 const acuminPro = localFont({
   src: [
@@ -38,6 +39,22 @@ export default function RootLayout({
           {children}
           <Footer />
         </Providers>
+        <Script
+          id='hotjar-analytics'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:6555514,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
       </body>
     </html>
   )
