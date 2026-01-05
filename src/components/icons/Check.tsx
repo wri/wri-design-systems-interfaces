c4f66a465e54a23b3eed014cfd79443561ebcd26
José Carlos Pesina Siller <josecarlospesinasiller@Joses-MacBook-Air.local>
2026-01-05 20:30:53 +0200

    DS-252-tabular_interfaces

---
+++ src/components/icons/Check.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react'
+
+import { Icon, IconProps } from '@chakra-ui/react'
+
+export const CheckIcon = (props: IconProps) => (
+  <Icon {...props}>
+    <svg viewBox='0 0 12 12' height='12px' width='12px'>
+      <path
+        d='M4.19632 10.4277L0 6.23139L1.04908 5.18231L4.19632 8.32955L10.9509 1.57495L12 2.62403L4.19632 10.4277Z'
+        fill='currentColor'
+      />
+    </svg>
+  </Icon>
+)
